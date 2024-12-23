143eccf0844bdc7ef0ca53cfb8a54ccc8033f904
matheusadriel2 <matheusadrieldsc@gmail.com>
2024-12-23 16:38:50 -0300

    feat: add the use of swiper and google icons libraries

---
+++ scripts/swiper.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".card-wrapper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 50,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
